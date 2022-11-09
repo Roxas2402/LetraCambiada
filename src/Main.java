111b3b4bc80c183d73e13365779ad9b81e960824
roxas <jeropastrana@gmail.com>
2022-11-09 17:01:54 +0100

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        char[] letras = sc.nextLine().toCharArray();
+        //char prohibida = "e";
+
+        for (int i = 0; i < letras.length; i++) {
+
+            if (letras[i] == 'e'){
+                letras[i] = 'a';
+            }
+        }
+
+        String frase = new String(letras);
+        System.out.println(frase);
     }
 }
